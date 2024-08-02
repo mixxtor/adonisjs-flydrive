2cca3b89fa9f46e3f69be80a185eb85603fe9e6c
mixxtor <113852548+mixxtor@users.noreply.github.com>
2024-08-02 14:24:41 +0700

    update index

--- index.ts
+++ index.ts
@@ -8,3 +8,7 @@
 */
 
 export { configure } from './configure.js'
+export { stubsRoot } from './stubs/main.js'
+export { defineConfig } from './src/utils/define_config.js'
+export * as provider from './providers/flydrive_provider.js'
+export * as flydrive from './services/main.js'
